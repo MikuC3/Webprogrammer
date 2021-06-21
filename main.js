44f8c6abe2366e9adb4f0777394b9fcc437ac9e2
Worachit Toto <Worachit8112@gmail.com>
2021-06-21 10:20:04 +0700

    ตัวแปรแบบต่างๆ

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-        let x =15;
-        let y =8;
-        let z = x + y;
+//let show = "RMUTL"
+//let name = "Worachit Toto"
+     
+//console.log(Javascript)
+//console.log(show)
+//console.log(name)
 
-        document.getElementById("test").innerHTML = y + y;
-        document.getElementById("test2").innerHTML = x * 10;
-        document.getElementById("test3").innerHTML = z;
+
+//document.getElementById("show").innerHTML = show;
+        
+let name = "Worachot"
+let lastname;
+lastname = "Toto"
+
+//console.log(`สวัสดีคุณ ${name} ${lastname}`);
+
+const PI = 3.14;
+
+let obj = {
+    name: "Worachit"
+    lastname: "Toto"
+    address: {
+        number: "18/6",
+        proovince: "พิษณุโลก"
+    }
+}
+
+console.log(obj.name);
+console.log(obj.lastname);
+console.log(obj.address.number);
+console.log(obj.address.proovince);
