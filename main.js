7c77f888fcd6e100fddff85c440972e0b3169808
Worachit Toto <Worachit8112@gmail.com>
2021-06-21 11:29:58 +0700

    Spread+Destrucring

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-let x = 10;
-
-function show() {
-    let x = 20;
-    return x;
+let obj = {
+    name: "Worachit",
+    lastname: "Toto",
+    age: 20,
+    Salary: 2000,
 }
 
-console.log(x);
-console.log(show());
-console.log(x);
+let obj2 = {
+    name: "Alen",
+    lastname: "Smith",
+    age: 25,
+    Salary: 3000,
+}
+
+const showdata = ({name, lastname, arg, salary}) =>`สวัสดีคุณ ${name} ${lastname}
+ขณะนี้คุณมีอายุ ${age}
+เงินเดือนของคุณเท่ากับ ${salary}`;
+
+console.log(showdata(obj));
+console.log(showdata(obj2));
+
+
+const fun = (x, y, z) => x + y + z;
+
+let num = [5, 6, 7];
+
+let res = fun(...num);
+
+console.log(res);
