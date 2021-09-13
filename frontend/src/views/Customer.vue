a40cfcb5c547e6e6ed8c3e9b2b09ab3bc43cca7f
Worachit Toto <Worachit8112@gmail.com>
2021-09-13 09:37:04 +0700

    del

---
+++ frontend/src/views/Customer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <h1>
+          
+      </h1> 
+      <HelloWorld msg="ลูกค้า (VIP)"/>
+      <HelloWorld msg="ลูกค้า (ระดับ 1)"/>
+      <HelloWorld msg="ลูกค้า (ระดับ 2)"/>
+      <HelloWorld msg="ลูกค้า (ระดับ 3)"/>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from "../components/HelloWorld.vue";
+
+
+export default {
+
+components:{
+HelloWorld
+}
+}
+</script>
+
+<style>
+
+</style>
