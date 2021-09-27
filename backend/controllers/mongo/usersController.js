97a8edc209d4811c076a5074b7be7c30e6b5c97e
Worachit Toto <Worachit8112@gmail.com>
2021-09-27 12:02:13 +0700

    Login Home

--- backend/controllers/mongo/usersController.js
+++ backend/controllers/mongo/usersController.js
@@ -1,31 +1,28 @@
 const users = require("../../models/mongo/users");
-
+// CRUD
+// R-Read อ่านข้อมูล
 exports.index = async (req, res, next) => {
-
     let data = await users.find();
-    
     res.status(200).json(data);
- 
 };
 
+// C-Create เขียนข้อมูล เพิ่มข้อมูล
 exports.insert = async (req, res, next) => {
-
-    let data = new users( {
+    let data = new users({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
     });
-
     data.save();
-
-    res.status(200).json({
+    res.status(201).json({
         message: "บันทึกข้อมูลเรียบร้อยแล้ว"
     });
 };
 
+// U-Update แก้ไขข้อมูล
 exports.update = async (req, res, next) => {
 
-    const id = "60ebbc69f217432aa40790bd";
+    const id = "60ebc223051284187c4191a9";
 
     const data = {
         username: "thailand",
@@ -33,8 +30,7 @@ exports.update = async (req, res, next) => {
     }
 
     let update = await users.updateOne(
-
-        {_id: id},
+        { _id: id },
         {
             username: data.username,
             email: data.email
@@ -49,27 +45,58 @@ exports.update = async (req, res, next) => {
         res.status(200).json({
             data: "แก้ไขข้อมูลเรียบร้อยแล้ว"
         });
-           
     }
 
 };
 
+// D-Delete ลบข้อมูล
 exports.delete = async (req, res, next) => {
 
-    const id = "610754cb8ccdec0900f29f65";
+    const id = "60ebc73ea54dca1ab964633d";
 
     const data = await users.deleteOne(
-        {_id: id}
+        { _id: id }
     );
 
-    if(data.deleteCount === 0){
+    if(data.deletedCount === 0){
         res.status(400).json({
             error: "ไม่สามารถลบข้อมูลได้"
         });
     } else {
-        res.status(400).json({
-            massage: "ลบข้อมูลเรียบร้อยแล้ว"
+        res.status(200).json({
+            message: "ลบข้อมูลเรียบร้อยแล้ว"
         });
     }
 
+};
+
+// Login การตรวจสอบเข้าสู่ระบบ
+exports.login = async (req, res, next) => {
+    // console.log(req.body);
+    // console.log(req.body.username);
+    // console.log(req.body.password);
+
+    // select * from users where username = "eeee" and password = "eeee"
+    let data = await users.find({ $and: [ 
+            { username: req.body.username },
+            { password: req.body.password } 
+        ]});
+
+    if(data.length > 0){
+        // console.log("มีข้อมูล");
+        res.status(200).json({
+            username: data[0].username,
+            email: data[0].email,
+            token: "dsafghjocmmisiohoghikjpdg",
+            status: 1,
+            message: "เข้าสู่ระบบเรียบร้อย"
+        });
+    } else {
+        // console.log("ไม่มีข้อมูล");
+        res.status(200).json({
+            status: 0,
+            message: "ชื่อผู้ใช้ หรือ รหัสผ่านไม่ถูกต้อง"
+        })
+    }
+    
 };
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="container">
-      <h1>webprogramming</h1>
-      <h2>วรชิต โต๊ะโต</h2>
-      <button type="button" class="btn btn-primary">Home</button>
-      <button type="button" class="btn btn-secondary">Secondary</button>
+  <div class="container-fluid">
+      <h1>Webprogramming</h1>
+      <h2>สวัสดีคุณ {{ username }} ({{ email }})</h2>
+      
+      <button @click="logout" type="button" class="btn btn-danger">Logout</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  mounted() {
+    this.email = localStorage.getItem("email");
+    this.username = localStorage.getItem("username");
+  },
+  data() {
+    return {
+      email: null,
+      username: null
+    }
+  },
+  methods: {
+    logout(){
+      localStorage.clear();
+      this.$router.push({ name: "Login" });
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
